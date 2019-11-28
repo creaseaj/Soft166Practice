a86bea9595c01c865d6de8129c013b5d4a8bbb2e
creaseaj <creaseaj@gmail.com>
2019-11-28 11:32:37 +0000

    JS calculator being added

--- javaScriptFiles/myScript1.js
+++ javaScriptFiles/myScript1.js
@@ -1,3 +1,10 @@
 function ShowAlert() {
     alert("This is an alert!")
 }
+
+function runCalc(){
+    alert("Maths calculator");
+    var int1 = document.getElementById("int1");
+    var int2 = document.getElementById("int2");
+
+}
